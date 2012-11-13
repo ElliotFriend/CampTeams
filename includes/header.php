580005bd307ba00169d67d81af79aa8c86dca781
Elliot John Voris <elliot@voris.me>
2012-11-13 17:58:35 -0600

    begin filling the files with stuff

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>Camp Teams - Save Yourself Time</title>
+    <link rel="stylesheet" href="includes/style.css" />
+</head>
+
+<body>
+    <!-- set up a Navigation Bar -->
+    <ul id="nav">
+        <li><a href="about.php">About</a></li>
+        <li><a href="collge.php">For Colleges</a></li>
+        <li><a href="request.php">Request a Team</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+    </ul>
