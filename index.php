cf154c228a73bccab397b80693c7f3a584a6d2fe
Elliot John Voris <elliot@voris.me>
2012-11-15 14:12:53 -0600

    added to index, created admin dir

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     include('includes/header.php');
 ?>
 
-<p>If you're from a college, you can <a href="register.php">register a college</a></p>
+<p>If you're from a college, you can <a href="register.php">register a college</a> that provides camp teams.</p>
 
-<p>If you're preparing for a camp, you can <a href="request.php">request camp teams</a></p>
+<p>If you're preparing for a camp, you can <a href="request.php">request camp teams</a> from any of the colleges that have registered</p>
 
 <?php
     include('includes/footer.php');
