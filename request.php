6c37c4e3b651a2195bd827793c303c88341ac272
Elliot John Voris <elliot@voris.me>
2012-11-14 18:03:53 -0600

    began request form

--- request.php
+++ request.php
@@ -1,7 +1,19 @@
 <?php
     require_once('includes/connectvars.php');
     include('includes/header.php');
+
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 ?>
+
+<p>Request a Camp Team from any of the colleges below</p>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="contactfirst">Contact First Name:</label>
+    <input type="text" id="contactfirst" name="contactfirst" /><br />
+    <label for="contactlast">Contact Last Name:</label>
+    <input type="text" id="contactlast" name="contactlast" /><br />
+    <label for="contactfirst">Contact First Name:</label>
+    <input type="text" id="contactfirst" name="contactlast" /><br />
+</form>
 
 
 
