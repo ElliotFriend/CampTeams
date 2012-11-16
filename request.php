1692017ccab54a6af9870d6d1c8ab605ce424e73
Elliot John Voris <elliot@voris.me>
2012-11-15 22:46:31 -0600

    Refined request_form -- including hidden dean's section

--- request.php
+++ request.php
@@ -1,12 +1,18 @@
 <?php
     require_once('includes/connectvars.php');
     include('includes/header.php');
-
+    // Make a connection to the database
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+    if (isset($_POST['submit'])) {
+        // set alert to empty, just in case one already exists
+        $alert = '';
+
+    }
 
     $greeting = "<p>Request a Camp Team from any of the colleges below</p><p>* Required</p>";
 
     echo $greeting;
-    include('includes/request_form.php');
+    include('includes/request_form.html');
     include('includes/footer.php');
 ?>
